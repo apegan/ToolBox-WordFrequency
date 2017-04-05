fc799b00e1e6bf9736c69cf46a5adf1abdf5ba9c
Alisha P <apegan@wellesley.edu>
2017-04-05 00:09:46 -0400

    turning in word frequency toolbox

--- frequency.py
+++ frequency.py
@@ -5,6 +5,7 @@ Analyzes the word frequencies of words in The Jungle
 """
 
 import string
+from collections import Counter
 with open('theJungle.txt', 'r') as f:
     lines = f.readlines()
 f.close
@@ -20,11 +21,17 @@ def get_word_list(file_name):
     while file_name[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
         curr_line += 1
     HCstrip = file_name[curr_line+1:]
-    WPstrip = [line.strip(' \n\t\r') for line in HCstrip]
-    PuncStrip = [s.translate(str.maketrans('', '', string.punctuation))
-                 for s in WPstrip]
-    lowcase = [text.lower() for text in PuncStrip]
-    word_list = [line.split(' ') for line in lowcase]
+    lines = []
+    for line in HCstrip:
+        space = line.strip(' \n\t\r')
+        punc = space.translate(str.maketrans('', '', string.punctuation))
+        lowcase = punc.lower()
+        lines.append(lowcase)
+    words = [line.split(' ') for line in lines]
+    word_list = []
+    for line in words:
+        for word in line:
+            word_list.append(word)
     return word_list
 
 
@@ -38,9 +45,13 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequently occurring
     """
-    pass
+    wordfreq = Counter(word_list)
+    return (n, wordfreq.most_common(n))
 
 
 if __name__ == "__main__":
     f = get_word_list(lines)
-    print(f)
+    n, w = get_top_n_words(f, 10)
+    print('The Top %s Words:' % (n,))
+    for letter, count in w:
+        print('%s: %d' % (letter, count))
